c445d41f0d5d2d3f2965f242cf7278528030ee7d
Dave <Dave@Dave-PC>
2012-12-24 23:32:57 +0100

    added security package

---
+++ src/security/HMAC.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package security;
+
+/**
+ * 
+ * @author Dave
+ */
+public class HMAC {
+    
+    public void appendHmac(String hash){
+        
+    }
+}
