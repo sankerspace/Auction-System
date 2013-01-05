77f2a27bb82c8feca287cce3d7c1070540112e2a
sanker <sanker@UbuntuHome.(none)>
2013-01-05 13:24:54 +0100

    Change main classes location

---
+++ src/AuctionClient.java
@@ -0,0 +1,15 @@
+package Main;
+
+import auctionmanagement.PartBClient;
+
+/**
+ *
+ * @author sanker
+ */
+public class AuctionClient {
+    public static void main(String[] args) {
+       PartBClient client=new PartBClient(args);
+       int code = client.run();
+       System.exit(code); 
+    }
+}
